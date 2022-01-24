6a94e4c5343c29bf83746339ff433b24f7990ff8
KusalNadeera <96426502+KusalNadeera@users.noreply.github.com>
2022-01-24 21:01:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ import os
 class Config:   
     API_ID = int(3413890)
     API_HASH = "4d309f9bb03ed141f5528b32f7866ad3"
-    BOT_TOKEN = "1960322648:AAHo_R8s0Abbb7SpbiHJabGJyUFZ55oyFfE"
+    BOT_TOKEN = "1960322648:AAFqbJZQhwrMvQuocvlibqiVYLx7Cn2Lqpo"
